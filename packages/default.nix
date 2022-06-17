bc10d68e075ddf5e9d0712db0460445d321f661c
Baitinq <manuelpalenzuelamerino@gmail.com>
2022-06-17 22:11:44 +0200

    Added anime-downloader package

--- packages/default.nix
+++ packages/default.nix
@@ -3,6 +3,6 @@ final: prev:
   custom.smart-wallpaper = prev.callPackage ./smart-wallpaper { };
   custom.dwmbar = prev.callPackage ./dwmbar { };
   custom.xwinwrapr = prev.callPackage ./xwinwrap { };
-  custom.anime-dl = prev.callPackage ./anime-dl { };
   custom.adl = prev.callPackage ./adl { };
+  custom.anime-downloader = prev.callPackage ./anime-downloader { pkgs = prev; };
 }
