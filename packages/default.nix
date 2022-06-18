2df8136e5df3c7fa075cb524101541220c726c5c
Baitinq <manuelpalenzuelamerino@gmail.com>
2022-06-18 13:42:44 +0200

    Renamed custom.xwinwrapr package to custom.xwinwrap

--- packages/default.nix
+++ packages/default.nix
@@ -2,7 +2,7 @@ final: prev:
 {
   custom.smart-wallpaper = prev.callPackage ./smart-wallpaper { };
   custom.dwmbar = prev.callPackage ./dwmbar { };
-  custom.xwinwrapr = prev.callPackage ./xwinwrap { };
+  custom.xwinwrap = prev.callPackage ./xwinwrap { };
   custom.trackma = prev.callPackage ./trackma { };
   custom.anime-downloader = prev.callPackage ./anime-downloader { pkgs = prev; };
   custom.adl = prev.callPackage ./adl { anime-downloader = final.custom.anime-downloader; trackma = final.custom.trackma; };
