ba15d2c1205f133a6c7f91e9df1cec9f03cb413f
Baitinq <manuelpalenzuelamerino@gmail.com>
2022-06-18 21:24:23 +0200

    Added patched kcc and kindlegen

--- packages/default.nix
+++ packages/default.nix
@@ -6,4 +6,5 @@ final: prev:
   custom.trackma = prev.callPackage ./trackma { };
   custom.anime-downloader = prev.callPackage ./anime-downloader { pkgs = prev; };
   custom.adl = prev.callPackage ./adl { anime-downloader = final.custom.anime-downloader; trackma = final.custom.trackma; };
+  custom.kindlegen = prev.callPackage ./kindlegen { };
 }
