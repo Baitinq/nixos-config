f37e67e360011cb90f9dc45a5cefbe2e06a8846d
Baitinq <manuelpalenzuelamerino@gmail.com>
2022-06-19 22:06:55 +0200

    Added NVIDIA drivers for phobos

--- hosts/phobos/hardware.nix
+++ hosts/phobos/hardware.nix
@@ -25,9 +25,9 @@ in
 
   powerManagement.cpuFreqGovernor = powerMode;
 
-  #services.xserver.videoDrivers = [ "nvidia" ];
   services.xserver = {
-    videoDrivers = [ "intel" ];
+    videoDrivers = [ "nvidia" ];
+
     # Enable touchpad support (enabled default in most desktopManager).
     libinput.enable = true;
   };
@@ -38,16 +38,18 @@ in
       driSupport = true;
     };
 
-    # nvidia.modesetting.enable = true;
-    /*nvidia.prime = {
-      sync.enable = true;
+    nvidia = {
+      modesetting.enable = true;
+      prime = {
+        sync.enable = true;
 
-      # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
-      nvidiaBusId = "PCI:1:0:0";
+        # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
+        nvidiaBusId = "PCI:1:0:0";
 
-      # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
-      intelBusId = "PCI:0:2:0";
-      };*/
+        # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
+        intelBusId = "PCI:0:2:0";
+      };
+    };
   };
 
 }
