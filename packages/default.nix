bbcfd6994a6c3fabd9672dad2cff52e1bb5119c6
Manuel Palenzuela <manuelpalenzuelamerino@gmail.com>
2022-06-20 17:36:59 +0200

    Indent custom packages under custom set

--- packages/default.nix
+++ packages/default.nix
@@ -1,9 +1,11 @@
 final: prev:
 {
-  custom.smart-wallpaper = prev.callPackage ./smart-wallpaper { };
-  custom.dwmbar = prev.callPackage ./dwmbar { };
-  custom.trackma = prev.callPackage ./trackma { };
-  custom.anime-downloader = prev.callPackage ./anime-downloader { pkgs = prev; };
-  custom.adl = prev.callPackage ./adl { anime-downloader = final.custom.anime-downloader; trackma = final.custom.trackma; };
-  custom.kindlegen = prev.callPackage ./kindlegen { };
+  custom = {
+    smart-wallpaper = prev.callPackage ./smart-wallpaper { };
+    dwmbar = prev.callPackage ./dwmbar { };
+    trackma = prev.callPackage ./trackma { };
+    anime-downloader = prev.callPackage ./anime-downloader { pkgs = prev; };
+    adl = prev.callPackage ./adl { anime-downloader = final.custom.anime-downloader; trackma = final.custom.trackma; };
+    kindlegen = prev.callPackage ./kindlegen { };
+  };
 }
