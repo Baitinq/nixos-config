bccc0e5a4cbaf992ab91f85f284cf82371af945d
Baitinq <manuelpalenzuelamerino@gmail.com>
2022-06-22 16:11:39 +0200

    Custom packages: anime-downloader: pass pkgs individually

--- packages/default.nix
+++ packages/default.nix
@@ -4,7 +4,7 @@ final: prev:
     smart-wallpaper = prev.callPackage ./smart-wallpaper { };
     dwmbar = prev.callPackage ./dwmbar { };
     trackma = prev.callPackage ./trackma { };
-    anime-downloader = prev.callPackage ./anime-downloader { pkgs = prev; };
+    anime-downloader = prev.callPackage ./anime-downloader { };
     adl = prev.callPackage ./adl { anime-downloader = final.custom.anime-downloader; trackma = final.custom.trackma; };
     kindlegen = prev.callPackage ./kindlegen { };
   };
