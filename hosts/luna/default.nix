4b263bed96b1b9e9a63f568b6a88400f34109e7e
Baitinq <manuelpalenzuelamerino@gmail.com>
2022-06-26 22:38:01 +0200

    Luna: Enable distributed builds

--- hosts/luna/default.nix
+++ hosts/luna/default.nix
@@ -34,5 +34,22 @@
   environment.systemPackages = with pkgs; [
     xf86_input_cmt #chromebook touchpad drivers
   ];
+
+  nix = {
+    distributedBuilds = true;
+    # optional, useful when the builder has a faster internet connection than yours
+    extraOptions = "builders-use-substitutes = true";
+    buildMachines = [{
+      hostName = "builder";
+      system = "x86_64-linux";
+      # if the builder supports building for multiple architectures,
+      # replace the previous line by, e.g.,
+      # systems = ["x86_64-linux" "aarch64-linux"];
+      maxJobs = 4;
+      speedFactor = 2;
+      supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
+      mandatoryFeatures = [ ];
+    }];
+  };
 }
 
