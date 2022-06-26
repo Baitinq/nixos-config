3b4e7e5e902fcef0511ad852023ff6cef4008071
Baitinq <manuelpalenzuelamerino@gmail.com>
2022-06-26 13:37:26 +0200

    Luna: Add hardware acceleration

--- hosts/luna/hardware.nix
+++ hosts/luna/hardware.nix
@@ -53,6 +53,12 @@ in
     opengl = {
       enable = true;
       driSupport = true;
+      extraPackages = with pkgs; [
+        intel-media-driver # LIBVA_DRIVER_NAME=iHD
+        vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+        vaapiVdpau
+        libvdpau-va-gl
+      ];
     };
   };
 
