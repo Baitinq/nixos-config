096131a1ce93648f54971858dc2def9c219bb60a
Baitinq <manuelpalenzuelamerino@gmail.com>
2022-06-26 13:37:26 +0200

    Luna: Add hardware acceleration

--- hosts/luna/home.nix
+++ hosts/luna/home.nix
@@ -12,26 +12,39 @@
     kcc
   ];
 
-  xdg.configFile."dwmbar/config".text = ''
-    #!/bin/sh
+  #TODO: Can we just not update prev set?
+  programs.firefox.profiles.default.settings = {
+    "general.autoScroll" = true; #TODO: This is aready set in parent home
+    "media.ffmpeg.vaapi.enabled" = true; #Hardware acceleration
+  };
 
-    # What modules, in what order
-    MODULES="weather wifi internet volume ram_perc cpuload cputemp battery date time"
+  xdg.configFile = {
+    "dwmbar/config".text = ''
+      #!/bin/sh
 
-    # Modules that require an active internet connection
-    ONLINE_MODULES="weather internet"
+      # What modules, in what order
+      MODULES="weather wifi internet volume ram_perc cpuload cputemp battery date time"
 
-    # Delay between showing the status bar
-    DELAY="0.05"
+      # Modules that require an active internet connection
+      ONLINE_MODULES="weather internet"
 
-    # Where the custom modules are stored
-    CUSTOM_DIR="/home/$USER/.config/dwmbar/modules/custom/"
+      # Delay between showing the status bar
+      DELAY="0.05"
 
-    # Separator between modules
-    SEPARATOR=" | "
+      # Where the custom modules are stored
+      CUSTOM_DIR="/home/$USER/.config/dwmbar/modules/custom/"
 
-    # Padding at the end and beggining of the status bar
-    RIGHT_PADDING=" "
-    LEFT_PADDING=" "
-  '';
+      # Separator between modules
+      SEPARATOR=" | "
+
+      # Padding at the end and beggining of the status bar
+      RIGHT_PADDING=" "
+      LEFT_PADDING=" "
+    '';
+
+    "mpv/mpv.conf".text = ''
+      ytdl-format=bestvideo[height<=?720][fps<=?60][vcodec!=?vp9]+bestaudio/best
+      hwdec
+    '';
+  };
 }
