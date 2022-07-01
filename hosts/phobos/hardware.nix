9d24a267949be64f217e7de97ca12a272141364c
Baitinq <manuelpalenzuelamerino@gmail.com>
2022-07-01 12:12:35 +0200

    Change synaptics sensibility

--- hosts/phobos/hardware.nix
+++ hosts/phobos/hardware.nix
@@ -49,7 +49,11 @@ in
       # Enable touchpad support (enabled default in most desktopManager).
       synaptics = {
         enable = true;
+        palmDetect = true;
         twoFingerScroll = true;
+        minSpeed = "1.0";
+        maxSpeed = "1.12";
+        accelFactor = "0.01";
       };
     };
   };
