6d493ac1ea6b1f22d86e06ca1bd261a4c116ac45
Baitinq <manuelpalenzuelamerino@gmail.com>
2022-07-24 17:52:52 +0200

    Set XDG env variables

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -68,6 +68,18 @@
     VISUAL = "nvim";
   };
 
+  environment.sessionVariables = rec {
+    XDG_CACHE_HOME = "\${HOME}/.cache";
+    XDG_CONFIG_HOME = "\${HOME}/.config";
+    XDG_BIN_HOME = "\${HOME}/.local/bin";
+    XDG_DATA_HOME = "\${HOME}/.local/share";
+    XDG_STATE_HOME = "\${HOME}/.local/state";
+
+    PATH = [
+      "\${XDG_BIN_HOME}"
+    ];
+  };
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
