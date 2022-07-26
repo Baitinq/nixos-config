443486ff4562f0755a290408cf6394e8cd6f52e5
Baitinq <manuelpalenzuelamerino@gmail.com>
2022-07-26 11:40:15 +0200

    Disable explicit DHCP
    This caused problems with spawning 2 DHCP instances and locking up the
    shutdown process

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -37,12 +37,6 @@
       # networking.firewall.allowedUDPPorts = [ ... ];
       # Or disable the firewall altogether.
     };
-    # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-    # (the default) this is the recommended approach. When using systemd-networkd it's
-    # still possible to use this option, but it's recommended to use it in conjunction
-    # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-    useDHCP = lib.mkDefault true;
-    # interfaces.enp0s3.useDHCP = lib.mkDefault true;
   };
 
   # Select internationalisation properties.
