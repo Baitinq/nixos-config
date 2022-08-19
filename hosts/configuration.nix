55b56ccaf01088639d23b7c034d02bbe19e3011a
Baitinq <manuelpalenzuelamerino@gmail.com>
2022-08-20 00:04:05 +0200

    Remove default packages

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -101,6 +101,8 @@
     comma
   ];
 
+  environment.defaultPackages = [ ];
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
