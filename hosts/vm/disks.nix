eff115ae44eec7107ec786d700f1f77c1f2552b6
Baitinq <manuelpalenzuelamerino@gmail.com>
2022-08-22 02:23:30 +0200

    Separate disk related hardware config into disks.nix

---
+++ hosts/vm/disks.nix
@@ -0,0 +1,45 @@
+{ config, lib, inputs, pkgs, modulesPath, ... }:
+{
+  fileSystems."/" = {
+    device = "none";
+    fsType = "tmpfs";
+  };
+
+  boot.initrd.luks.devices."encrypted_boot".device = "/dev/disk/by-partlabel/boot";
+
+  fileSystems."/boot" = {
+    device = "/dev/mapper/encrypted_boot";
+    fsType = "vfat";
+  };
+
+  fileSystems."/boot/efi" = {
+    device = "/dev/disk/by-partlabel/efi";
+    fsType = "vfat";
+  };
+
+  boot.initrd.luks.devices."encrypted_root".device = "/dev/disk/by-partlabel/root";
+
+  fileSystems."/nix" = {
+    device = "/dev/mapper/encrypted_root";
+    fsType = "btrfs";
+    options = [ "subvol=nix" "compress-force=zstd" "noatime" ];
+  };
+
+  fileSystems."/persist" = {
+    device = "/dev/mapper/encrypted_root";
+    fsType = "btrfs";
+    neededForBoot = true;
+    options = [ "subvol=persist" "compress-force=zstd" "noatime" ];
+  };
+
+  fileSystems."/home" = {
+    device = "/dev/mapper/encrypted_root";
+    fsType = "btrfs";
+    options = [ "subvol=home" "compress-force=zstd" ];
+  };
+
+  swapDevices = [ ];
+
+  zramSwap.enable = true;
+
+}
--- hosts/vm/hardware.nix
+++ hosts/vm/hardware.nix
@@ -1,6 +1,8 @@
-{ config, lib, pkgs, modulesPath, ... }:
+{ config, lib, inputs, pkgs, modulesPath, ... }:
 {
-  imports = [ ];
+  imports = [
+    ./disks.nix
+  ];
 
   boot = {
     initrd = {
@@ -12,46 +14,6 @@
     extraModulePackages = [ ];
     kernelParams = [ "net.ifnames=0" "biosdevname=0" "mitigations=off" ];
   };
-
-  fileSystems."/" = {
-    device = "none";
-    fsType = "tmpfs";
-  };
-
-  boot.initrd.luks.devices."encrypted_boot".device = "/dev/disk/by-partlabel/boot";
-
-  fileSystems."/boot" = {
-    device = "/dev/mapper/encrypted_boot";
-    fsType = "vfat";
-  };
-
-  fileSystems."/boot/efi" = {
-    device = "/dev/disk/by-partlabel/efi";
-    fsType = "vfat";
-  };
-
-  boot.initrd.luks.devices."encrypted_root".device = "/dev/disk/by-partlabel/root";
-
-  fileSystems."/nix" = {
-    device = "/dev/mapper/encrypted_root";
-    fsType = "btrfs";
-    options = [ "subvol=nix" "compress-force=zstd" "noatime" ];
-  };
-
-  fileSystems."/persist" = {
-    device = "/dev/mapper/encrypted_root";
-    fsType = "btrfs";
-    neededForBoot = true;
-    options = [ "subvol=persist" "compress-force=zstd" "noatime" ];
-  };
-
-  fileSystems."/home" = {
-    device = "/dev/mapper/encrypted_root";
-    fsType = "btrfs";
-    options = [ "subvol=home" "compress-force=zstd" ];
-  };
-
-  swapDevices = [ ];
 
   services.xserver = {
     # Enable touchpad support (enabled default in most desktopManager).
