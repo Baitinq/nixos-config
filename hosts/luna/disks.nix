af11fb1e88c2c5f3c17f4b600db0e87126f7f9de
Baitinq <manuelpalenzuelamerino@gmail.com>
2022-08-23 20:55:19 +0200

    Add temporary disks config

--- hosts/luna/disks.nix
+++ hosts/luna/disks.nix
@@ -5,46 +5,105 @@ let
     content = {
       "disk/by-id/mmc-AGND3R_0x48d44fdc" = {
         type = "table";
-        format = "msdos";
-        partitions = [{
-          type = "partition";
-          part-type = "primary";
-          start = "1M";
-          end = "100%";
-          bootable = true;
-          content = {
-            type = "filesystem";
-            format = "ext4";
-            mountpoint = "/";
-          };
-        }];
+        format = "gpt";
+        partitions = [
+          {
+            type = "partition";
+            part-type = "ESP";
+            start = "0";
+            end = "64M";
+            fs-type = "fat32";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot/esp";
+            };
+          }
+          {
+            type = "partition";
+            # leave space for the grub aka BIOS boot
+            start = "64M";
+            end = "264M";
+            part-type = "primary";
+            content = {
+              type = "luks";
+              name = "encrypted_boot";
+              keyfile = "/tmp/secret2.key";
+              extraArgs = [ "--type luks1" ];
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/boot";
+              };
+            };
+          }
+          {
+            type = "partition";
+            # leave space for the grub aka BIOS boot
+            start = "264M";
+            end = "100%";
+            part-type = "primary";
+            content = {
+              type = "luks";
+              name = "encrypted_nix";
+              keyfile = "/tmp/secret.key";
+              extraArgs = [ "--type luks2" ];
+              content = {
+                type = "filesystem";
+                format = "btrfs";
+                mountpoint = "/nix";
+              };
+            };
+          }
+
+        ];
       };
       "disk/by-id/usb-Generic_STORAGE_DEVICE_000000000208-0:0" = {
         type = "table";
-        format = "msdos";
-        partitions = [{
-          type = "partition";
-          part-type = "primary";
-          start = "1M";
-          end = "100%";
-          bootable = true;
-          content = {
-            type = "filesystem";
-            format = "ext4";
-            mountpoint = "/";
-          };
-        }];
+        format = "gpt";
+        partitions = [
+          {
+            type = "partition";
+            part-type = "primary";
+            start = "0";
+            end = "100%";
+            content = {
+              type = "luks";
+              name = "encrypted_home_and_persist";
+              keyfile = "/tmp/secret.key";
+              extraArgs = [ "--type luks2" ];
+              content = {
+                type = "lvm";
+                name = "pool";
+                lvs = {
+                  persist = {
+                    type = "lv";
+                    size = "4G";
+                    content = {
+                      type = "filesystem";
+                      format = "btrfs";
+                      mountpoint = "/persist";
+                    };
+                  };
+                  home = {
+                    type = "lv";
+                    size = "100%FREE";
+                    content = {
+                      type = "filesystem";
+                      format = "btrfs";
+                      mountpoint = "/home";
+                    };
+                  };
+                };
+              };
+            };
+          }
+        ];
       };
     };
   };
 in
 {
-  environment.systemPackages = with pkgs;[
-    parted
-    (pkgs.writeScriptBin "disko-create" (inputs.disko.lib.create partitionsConfig))
-    (pkgs.writeScriptBin "disko-mount" (inputs.disko.lib.mount partitionsConfig))
-  ];
-
   fileSystems."/" = {
     device = "none";
     fsType = "tmpfs";
@@ -95,4 +154,10 @@ in
 
   zramSwap.enable = true;
 
+
+  environment.systemPackages = with pkgs;[
+    parted
+    (pkgs.writeScriptBin "disko-create" (inputs.disko.lib.create partitionsConfig))
+    (pkgs.writeScriptBin "disko-mount" (inputs.disko.lib.mount partitionsConfig))
+  ];
 }
