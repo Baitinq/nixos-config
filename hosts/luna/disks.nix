278ba42c8fb1bb4f42bf8a0ce7b73e1179095a97
Baitinq <manuelpalenzuelamerino@gmail.com>
2022-08-25 22:14:07 +0200

    Implement disko as a module
    This allows us to build it independently when bootstraping a system

--- hosts/luna/disks.nix
+++ hosts/luna/disks.nix
@@ -1,4 +1,4 @@
-{ inputs, pkgs, ... }:
+{ inputs, lib, config, pkgs, ... }:
 let
   partitionsConfig = {
     type = "devices";
@@ -99,60 +99,79 @@ let
   };
 in
 {
-  fileSystems."/" = {
-    device = "none";
-    fsType = "tmpfs";
+  config = {
+
+    fileSystems."/" = {
+      device = "none";
+      fsType = "tmpfs";
+    };
+
+    boot.initrd.luks.devices."encrypted_boot" = {
+      device = "/dev/disk/by-uuid/4f5ba100-5c69-49ce-b0cf-2f219a5e9e51";
+      preLVM = true;
+    };
+
+    fileSystems."/boot" = {
+      device = "/dev/mapper/encrypted_boot";
+      fsType = "vfat";
+    };
+
+    fileSystems."/boot/efi" = {
+      device = "/dev/disk/by-uuid/BD51-1431";
+      fsType = "vfat";
+    };
+
+    boot.initrd.luks.devices."encrypted_nix".device = "/dev/disk/by-uuid/596e43d3-ccda-4f06-bce9-58d6a8c0dd79";
+
+    fileSystems."/nix" = {
+      device = "/dev/mapper/encrypted_nix";
+      fsType = "btrfs";
+      neededForBoot = true;
+      options = [ "subvol=nix" "compress-force=zstd" "noatime" ];
+    };
+
+    boot.initrd.luks.devices."encrypted_home_and_persist".device = "/dev/disk/by-uuid/47a8ddde-1237-4a0f-84c4-f17fbd22ea3f";
+
+    fileSystems."/persist" = {
+      device = "/dev/mapper/encrypted_home_and_persist";
+      fsType = "btrfs";
+      neededForBoot = true;
+      options = [ "subvol=persist" "compress-force=zstd" "noatime" ];
+    };
+
+    fileSystems."/home" = {
+      device = "/dev/mapper/encrypted_home_and_persist";
+      fsType = "btrfs";
+      options = [ "subvol=home" "compress-force=zstd" ];
+    };
+
+    services.btrfs.autoScrub.enable = true;
+
+    swapDevices = [ ];
+
+    zramSwap.enable = true;
+
+
+    environment.systemPackages = [
+      config.disko-create
+      config.disko-mount
+    ];
   };
 
-  boot.initrd.luks.devices."encrypted_boot" = {
-    device = "/dev/disk/by-uuid/4f5ba100-5c69-49ce-b0cf-2f219a5e9e51";
-    preLVM = true;
+  options.disko-create = with lib; mkOption {
+    type = types.package;
+    default = pkgs.buildEnv {
+      name = "disko-create";
+      paths = [ (pkgs.writeScriptBin "disko-create" (inputs.disko.lib.create partitionsConfig)) pkgs.parted ];
+    };
   };
 
-  fileSystems."/boot" = {
-    device = "/dev/mapper/encrypted_boot";
-    fsType = "vfat";
+  options.disko-mount = with lib; mkOption {
+    type = types.package;
+    default = pkgs.buildEnv {
+      name = "disko-mount";
+      paths = [ (pkgs.writeScriptBin "disko-mount" (inputs.disko.lib.mount partitionsConfig)) pkgs.parted ];
+    };
   };
 
-  fileSystems."/boot/efi" = {
-    device = "/dev/disk/by-uuid/BD51-1431";
-    fsType = "vfat";
-  };
-
-  boot.initrd.luks.devices."encrypted_nix".device = "/dev/disk/by-uuid/596e43d3-ccda-4f06-bce9-58d6a8c0dd79";
-
-  fileSystems."/nix" = {
-    device = "/dev/mapper/encrypted_nix";
-    fsType = "btrfs";
-    neededForBoot = true;
-    options = [ "subvol=nix" "compress-force=zstd" "noatime" ];
-  };
-
-  boot.initrd.luks.devices."encrypted_home_and_persist".device = "/dev/disk/by-uuid/47a8ddde-1237-4a0f-84c4-f17fbd22ea3f";
-
-  fileSystems."/persist" = {
-    device = "/dev/mapper/encrypted_home_and_persist";
-    fsType = "btrfs";
-    neededForBoot = true;
-    options = [ "subvol=persist" "compress-force=zstd" "noatime" ];
-  };
-
-  fileSystems."/home" = {
-    device = "/dev/mapper/encrypted_home_and_persist";
-    fsType = "btrfs";
-    options = [ "subvol=home" "compress-force=zstd" ];
-  };
-
-  services.btrfs.autoScrub.enable = true;
-
-  swapDevices = [ ];
-
-  zramSwap.enable = true;
-
-
-  environment.systemPackages = with pkgs;[
-    parted
-    (pkgs.writeScriptBin "disko-create" (inputs.disko.lib.create partitionsConfig))
-    (pkgs.writeScriptBin "disko-mount" (inputs.disko.lib.mount partitionsConfig))
-  ];
 }
