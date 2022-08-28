dff3b305a03ecc22b8a12a9e24ba98693245e482
Baitinq <30861839+Baitinq@users.noreply.github.com>
2022-08-28 22:11:24 +0000

    Implement non NixOS hosts support

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,4 +1,4 @@
-{ lib, nixpkgs, inputs, home-manager, ... }:
+{ lib, inputs, isNixOS, nixpkgs, home-manager, ... }:
 let
   user = "baitinq";
 
@@ -10,7 +10,7 @@ let
     { hostname = "vm"; system = "x86_64-linux"; timezone = secrets.main_timezone; location = secrets.main_location; }
   ];
 
-  mkHost = { hostname, system, timezone, location }:
+  mkHost = { hostname, system, timezone, location }: isNixOS:
     let
       pkgs = import nixpkgs {
         inherit system;
@@ -23,27 +23,37 @@ let
       };
       extraArgs = { inherit pkgs inputs user secrets hostname timezone location; };
     in
-    nixpkgs.lib.nixosSystem {
-      inherit system;
-      specialArgs = extraArgs;
-      modules = [
-        ./configuration.nix
-        ./${hostname}
-        home-manager.nixosModules.home-manager
+    if isNixOS
+    then
+      nixpkgs.lib.nixosSystem
         {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.extraSpecialArgs = extraArgs;
-          home-manager.users.${user} = {
-            imports = [ ./home.nix ] ++ [ (import ./${ hostname }/home.nix) ];
-          };
+          inherit system;
+          specialArgs = extraArgs;
+          modules = [
+            ./configuration.nix
+            ./${hostname}
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.extraSpecialArgs = extraArgs;
+              home-manager.users.${user} = {
+                imports = [ ./home.nix ] ++ [ (import ./${ hostname }/home.nix) ];
+              };
+            }
+          ];
         }
-      ];
-    };
+    else
+      home-manager.lib.homeManagerConfiguration
+        {
+          inherit pkgs;
+          extraSpecialArgs = extraArgs;
+          modules = [ ./home.nix ] ++ [ (import ./${ hostname }/home.nix) ];
+        };
 in
   /*
     We have a list of sets.
     Map each element of the list applying the mkHost function to its elements and returning a set in the listToAttrs format
     builtins.listToAttrs on the result
   */
-builtins.listToAttrs (map ({ hostname, system, timezone, location }: { name = hostname; value = mkHost { inherit hostname system timezone location; }; }) hosts)
+builtins.listToAttrs (map ({ hostname, system, timezone, location }: { name = hostname; value = mkHost { inherit hostname system timezone location; } isNixOS; }) hosts)
