1dbbdbbb62a64f8df2e1280b8939a6ccdd79acaa
Baitinq <manuelpalenzuelamerino@gmail.com>
2022-08-28 17:58:29 +0200

    Packages: Remove trackma
    Already in upstream

--- packages/default.nix
+++ packages/default.nix
@@ -3,8 +3,7 @@ final: prev:
   custom = {
     smart-wallpaper = prev.callPackage ./smart-wallpaper { };
     dwmbar = prev.callPackage ./dwmbar { };
-    trackma = prev.callPackage ./trackma { };
-    adl = prev.callPackage ./adl { inherit (final.custom) trackma; };
+    adl = prev.callPackage ./adl { };
     kindlegen = prev.callPackage ./kindlegen { };
     manga-cli = prev.callPackage ./manga-cli { };
     mov-cli = prev.callPackage ./mov-cli { };
