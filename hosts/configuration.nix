8f35e3722acb6844e70708626b9d7fb7976e5eed
Baitinq <manuelpalenzuelamerino@gmail.com>
2022-08-29 15:35:04 +0200

    Set dash as the default /bin/sh

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -86,6 +86,7 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     (uutils-coreutils.override { prefix = ""; })
+    dash
     lm_sensors
     pulseaudio # used for tools
     python
@@ -107,6 +108,8 @@
   ];
 
   environment.defaultPackages = [ ];
+
+  environment.binsh = "${pkgs.dash}/bin/dash";
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
