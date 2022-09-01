fa4008facdcacfb3d5edb58c414c288d5166dfde
Baitinq <manuelpalenzuelamerino@gmail.com>
2022-09-01 23:46:59 +0200

    Packages: dwmbar: Repackage dwmbar to not need patches

--- packages/dwmbar/default.nix
+++ packages/dwmbar/default.nix
@@ -1,21 +1,26 @@
-{ stdenv, lib, fetchFromGitHub, bash }:
+{ stdenv
+, lib
+, fetchFromGitHub
+}:
+
 stdenv.mkDerivation {
   pname = "dwmbar";
   version = "1.0";
+  
   src = fetchFromGitHub {
     owner = "thytom";
     repo = "dwmbar";
     rev = "574f5703c558a56bc9c354471543511255423dc7";
     sha256 = "sha256-IrelZpgsxq2dnsjMdh7VC5eKffEGRbDkZmZBD+tROPs=";
   };
-  patches = [ ../../patches/dwmbar.patch ];
-  buildInputs = [ bash ];
+  
   installPhase = ''
     mkdir -p $out/bin
-    mkdir -p $out/bin/_dwmbar/modules
-    cp -r modules $out/bin/_dwmbar/
-    install config $out/bin/_dwmbar/config
-    install bar.sh $out/bin/_dwmbar/bar.sh
+    mkdir -p $out/config
+    cp -r modules/ $out/config/
+    install config $out/config/config
+    install bar.sh $out/config/bar.sh
+    substituteInPlace dwmbar --replace 'DEFAULT_CONFIG_DIR="/usr/share/dwmbar"' "DEFAULT_CONFIG_DIR=\"$out/config\""
     install dwmbar $out/bin/dwmbar
   '';
 }
