c9cb073985bc93c88d92efa778b9deaba5c9a422
Baitinq <manuelpalenzuelamerino@gmail.com>
2022-09-06 17:41:26 +0200

    Module: Virtualisation: Add virtualbox

--- modules/virtualisation/default.nix
+++ modules/virtualisation/default.nix
@@ -4,7 +4,17 @@
   virtualisation = {
     docker.enable = true;
     libvirtd.enable = true;
+    virtualbox = {
+      host = {
+        enable = true;
+      };
+      guest = {
+        enable = true;
+        x11 = true;
+      };
+    };
   };
 
   users.users.${user}.extraGroups = [ "docker" "libvirtd" ];
+  users.extraGroups.vboxusers.members = [ "user-with-access-to-virtualbox" ];
 }
