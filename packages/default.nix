5ecc0525df64028aa59061a3f29d097b0b1ca454
Baitinq <manuelpalenzuelamerino@gmail.com>
2022-09-10 21:25:07 +0200

    Packages: Remove non-existant package references

--- packages/default.nix
+++ packages/default.nix
@@ -1,9 +1,7 @@
 final: prev:
 {
   custom = {
-    adl = prev.callPackage ./adl { };
     kindlegen = prev.callPackage ./kindlegen { };
-    xmonadctl = prev.callPackage ./xmonadctl { };
     lemacs = prev.callPackage ./lemacs { };
   };
 }
