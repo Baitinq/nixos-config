25d157b0469c9ad70045e1e66362a11a12d6c745
Baitinq <manuelpalenzuelamerino@gmail.com>
2022-09-10 21:21:25 +0200

    Packages: Remove manga-cli
    Already included in nixpkgs

--- packages/default.nix
+++ packages/default.nix
@@ -4,7 +4,6 @@ final: prev:
     smart-wallpaper = prev.callPackage ./smart-wallpaper { };
     adl = prev.callPackage ./adl { };
     kindlegen = prev.callPackage ./kindlegen { };
-    manga-cli = prev.callPackage ./manga-cli { };
     mov-cli = prev.callPackage ./mov-cli { };
     xmonadctl = prev.callPackage ./xmonadctl { };
     lemacs = prev.callPackage ./lemacs { };
