28665411a5192b956c5c83a65c960547e506b914
Baitinq <manuelpalenzuelamerino@gmail.com>
2022-09-26 19:34:06 +0200

    Module: Virtualisation: Replace docker with podman

--- modules/virtualisation/default.nix
+++ modules/virtualisation/default.nix
@@ -2,11 +2,14 @@
 
 {
   virtualisation = {
-    docker.enable = true;
+    podman = {
+      enable = true;
+      dockerCompat = true;
+    };
     libvirtd.enable = true;
     virtualbox.host.enable = true;
   };
 
-  users.users.${user}.extraGroups = [ "docker" "libvirtd" ];
+  users.users.${user}.extraGroups = [ "libvirtd" ];
   users.extraGroups.vboxusers.members = [ "user-with-access-to-virtualbox" ];
 }
