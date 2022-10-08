5a37cd1acbf722c037b58e26b62ad8cd80ca4a7a
Baitinq <manuelpalenzuelamerino@gmail.com>
2022-10-08 22:03:02 +0200

    Hardware: Disks: Persist ssh host key files

--- hardware/virtualbox/disks.nix
+++ hardware/virtualbox/disks.nix
@@ -56,6 +56,10 @@ in
       files = [
         "/etc/machine-id"
         "/etc/nix/id_rsa"
+        "/etc/ssh/ssh_host_rsa_key"
+        "/etc/ssh/ssh_host_rsa_key.pub"
+        "/etc/ssh/ssh_host_ed25519_key"
+        "/etc/ssh/ssh_host_ed25519_key.pub"
       ];
     };
 
