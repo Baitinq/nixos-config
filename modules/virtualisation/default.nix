a7a46d3cd392d782a118f4551b27479e8b2274d9
Baitinq <manuelpalenzuelamerino@gmail.com>
2022-10-14 16:59:28 +0200

    Module: Virtualisation: Add docker-compose for podman

--- modules/virtualisation/default.nix
+++ modules/virtualisation/default.nix
@@ -5,10 +5,16 @@
     podman = {
       enable = true;
       dockerCompat = true;
+      defaultNetwork.dnsname.enable = true;
     };
     libvirtd.enable = true;
     virtualbox.host.enable = true;
   };
+
+  environment.systemPackages = with pkgs; [
+    docker-compose
+    podman-compose
+  ];
 
   users.users.${user}.extraGroups = [ "libvirtd" ];
   users.extraGroups.vboxusers.members = [ "user-with-access-to-virtualbox" ];
