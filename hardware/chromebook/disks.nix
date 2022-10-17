29b5140a343448ed1873cc5225ffe656a3b13fda
Baitinq <manuelpalenzuelamerino@gmail.com>
2022-10-17 15:24:26 +0200

    Hardware: Disks: Add diff-root utility
    This utility allows to check what changed in the / from boot till the execution
    of the command. Useful for knowing what to persist.

--- hardware/chromebook/disks.nix
+++ hardware/chromebook/disks.nix
@@ -52,6 +52,22 @@ let
     mount -o compress-force=zstd /dev/mapper/encrypted_home_and_persist_pool-home /mnt/home
     mount -o compress-force=zstd,noatime /dev/mapper/encrypted_home_and_persist_pool-persist /mnt/persist
   '';
+
+  # Utility to save a snapshot of the root tree
+  save-root = pkgs.writers.writeDashBin "save-root" ''
+    ${pkgs.findutils}/bin/find \
+      / -xdev \( -path /tmp -o -path /var/tmp -o -path /var/log/journal \) \
+      -prune -false -o -print0 | sort -z | tr '\0' '\n' > "$1"
+  '';
+
+  # Utility to compare the root tree
+  diff-root = pkgs.writers.writeDashBin "diff-root" ''
+    export PATH=${with pkgs; lib.makeBinPath [ diffutils less ]}:$PATH
+    current="$(mktemp current-root.XXX --tmpdir)"
+    trap 'rm "$current"' EXIT INT HUP
+    ${save-root}/bin/save-root "$current"
+    diff -u /run/initial-root "$current" --color=always | ''${PAGER:-less -R}
+  '';
 in
 {
   config = {
@@ -128,7 +144,19 @@ in
       config.disks-create
       config.disks-format
       config.disks-mount
+
+      diff-root
     ];
+
+    systemd.services.save-root-snapshot = {
+      description = "save a snapshot of the initial root tree";
+      wantedBy = [ "sysinit.target" ];
+      requires = [ "-.mount" ];
+      after = [ "-.mount" ];
+      serviceConfig.Type = "oneshot";
+      serviceConfig.RemainAfterExit = true;
+      serviceConfig.ExecStart = ''${save-root}/bin/save-root /run/initial-root'';
+    };
   };
 
   options.disks-create = with lib; mkOption rec {
