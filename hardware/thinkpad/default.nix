4d7a00b4f61e93ec503ac2756873a33e71debc6c
Baitinq <manuelpalenzuelamerino@gmail.com>
2022-12-23 15:55:04 +0100

    Hardware: Thinkpad: Add instructions on ectool setup

--- hardware/thinkpad/default.nix
+++ hardware/thinkpad/default.nix
@@ -9,8 +9,14 @@
   # me_state = Disable
   # gfx_uma_size = 128M
 
+  # ectool battery threshold setting (84%):
+  #
+  # ectool -w 0xb1 -z 0x54
+
   environment.systemPackages = with pkgs; [
     flashrom
     nvramtool
+    ectool
   ];
+
 }
