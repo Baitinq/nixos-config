a82e5ed336b6f37ebfae8ee89b3ee96aab21c730
Baitinq <manuelpalenzuelamerino@gmail.com>
2022-12-23 13:10:02 +0100

    Hardware: Thinkpad: Add comments on how to setup nvramtool

--- hardware/thinkpad/default.nix
+++ hardware/thinkpad/default.nix
@@ -2,6 +2,13 @@
 {
   imports = [ ./hardware.nix ];
 
+  # nvramtool settings:
+  #
+  # fn_ctrl_swap = Enable
+  # wwan = Disable
+  # me_state = Disable
+  # gfx_uma_size = 128M
+
   environment.systemPackages = with pkgs; [
     flashrom
     nvramtool
