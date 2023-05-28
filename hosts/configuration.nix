ea015936315ed179c9cb836f4e042065f31b0c8e
Baitinq <manuelpalenzuelamerino@gmail.com>
2023-05-28 17:42:43 +0200

    Home: Add xdg-desktop-portal-gtk to the extra portals

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -130,6 +130,8 @@
   xdg.portal = {
     enable = true;
     wlr.enable = true;
+    # gtk portal needed to make gtk apps happy
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
   };
 
   #fix swaylock
