c6933d5e516ef2bf7e0254d83771dd185b899b11
Baitinq <manuelpalenzuelamerino@gmail.com>
2023-06-05 13:12:17 +0200

    Tests: Add initial flake test

---
+++ tests/flake_test.nix
@@ -0,0 +1,9 @@
+let
+  flake = import ../flake.nix;
+in [
+  {
+    name = "home-manger should follow nixpkgs";
+    actual = flake.inputs.home-manager.inputs.nixpkgs.follows;
+    expected = "nixpkgs";
+  }
+]
