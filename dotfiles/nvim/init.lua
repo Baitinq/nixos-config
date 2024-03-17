4b8e33bcb385469d57567f6aeb3fcfe6a9ffb69b
Baitinq <manuelpalenzuelamerino@gmail.com>
2024-03-17 23:29:25 +0100

    Dotfiles: Nvim: Priorize local lsp install

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -574,7 +574,7 @@ require('lazy').setup({
       --    :Mason
       --
       --  You can press `g?` for help in this menu
-      require('mason').setup()
+      require('mason').setup({ PATH = "append"})
 
       -- You can add other tools here that you want Mason to install
       -- for you, so that they are available from within Neovim.
