66779241df07d7d7ef17c8f6a7d0e31a1083ce1b
Baitinq <manuelpalenzuelamerino@gmail.com>
2024-03-31 17:17:47 +0200

    Dotfiles: Nvim: Use telescope git_files

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -390,7 +390,8 @@ require('lazy').setup({
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>sf', builtin.git_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>sF', function() builtin.find_files({previewer = false}) end, { desc = '[S]earch [F]iles (non-git)' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
