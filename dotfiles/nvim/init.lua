74e2bcf76016277e385cb3605df230db76ed52e1
Baitinq <manuelpalenzuelamerino@gmail.com>
2024-03-31 01:18:47 +0100

    Dotfiles: Nvim: Setup codelens

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -515,6 +515,7 @@ require('lazy').setup({
           -- Execute a code action, usually your cursor needs to be on top of an error
           -- or a suggestion from your LSP for this to activate.
           map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+          map('<leader>cl', vim.lsp.codelens.run, '[C]ode [L]ens')
 
           -- Opens a popup that displays documentation about the word under your cursor
           --  See `:help K` for why this keymap
@@ -523,6 +524,7 @@ require('lazy').setup({
           -- WARN: This is not Goto Definition, this is Goto Declaration.
           --  For example, in C this would take you to the header
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+
 
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
@@ -542,6 +544,12 @@ require('lazy').setup({
             vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
               buffer = event.buf,
               callback = vim.lsp.buf.clear_references,
+            })
+          end
+          if client and client.server_capabilities.codeLensProvider then
+            vim.api.nvim_create_autocmd({ 'BufEnter', 'CursorHold', 'InsertLeave' }, {
+              buffer = event.buf,
+              callback = vim.lsp.codelens.refresh,
             })
           end
         end,
