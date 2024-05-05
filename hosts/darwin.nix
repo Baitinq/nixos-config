f69fd001238bf5e080269abf0f07e528a0738027
Baitinq <manuelpalenzuelamerino@gmail.com>
2024-05-05 22:57:41 +0200

    fmt

--- hosts/darwin.nix
+++ hosts/darwin.nix
@@ -3,16 +3,16 @@
   users.users."${user}".home = "/Users/${user}";
 
   environment.systemPackages = with pkgs; [
-      neovim-nightly
-      ripgrep
-      fd
-      tmux
-      yt-dlp
-      pfetch
-      fzf
-      comma
-      moreutils
-    ];
+    neovim-nightly
+    ripgrep
+    fd
+    tmux
+    yt-dlp
+    pfetch
+    fzf
+    comma
+    moreutils
+  ];
 
   services.nix-daemon.enable = true;
 }
