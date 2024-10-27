56a670536a81b6188b5614edb52512d6cd0941b1
Baitinq <manuelpalenzuelamerino@gmail.com>
2024-10-27 11:35:31 +0100

    dotfiles: nvim: run autoformatter async and install haskell formatter

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -693,13 +693,12 @@ require('lazy').setup({
     'stevearc/conform.nvim',
     opts = {
       notify_on_error = false,
-      format_on_save = function(bufnr)
+      format_after_save = function(bufnr)
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
         local disable_filetypes = { c = true, cpp = true }
         return {
-          timeout_ms = 500,
           lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
         }
       end,
@@ -712,6 +711,7 @@ require('lazy').setup({
         -- is found.
         -- javascript = { { "prettierd", "prettier" } },
         go = { "goimports", "gofmt" },
+        haskell = { 'ormolu' },
       },
     },
   },
