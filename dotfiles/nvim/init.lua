20eaf3513f2d1378cd50f9bf96d377e083fe1066
Baitinq <manuelpalenzuelamerino@gmail.com>
2025-01-23 23:05:11 +0100

    dotfiles: nvim: add avante

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -241,6 +241,23 @@ require('lazy').setup({
   'github/copilot.vim',
 
   {
+    "yetone/avante.nvim",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      {
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = { file_types = { "markdown", "Avante" } },
+        ft = { "markdown", "Avante" },
+      },
+    },
+    build = "make",
+    opts = { provider = "copilot" },
+  },
+
+  {
     "linrongbin16/gitlinker.nvim",
     config = function()
       require('gitlinker').setup {
