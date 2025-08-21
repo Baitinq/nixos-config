136c39a6fae66cb6d45da30c2128626659f3609c
Baitinq <manuelpalenzuelamerino@gmail.com>
2025-08-22 00:32:08 +0200

    Hosts: Add cuda binary cache

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -233,6 +233,12 @@
       auto-allocate-uids = true;
       max-jobs = "auto";
       cores = 0;
+      substituters = [
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
     };
     optimise.automatic = true;
   };
