2e113471bbee02f102057cb77e4ac686c53034b6
Baitinq <manuelpalenzuelamerino@gmail.com>
2025-09-16 23:51:09 +0200

    Secrets: Add github access token

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -274,8 +274,10 @@
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
+      access-tokens = "github.com=${secrets.github_access_token}";
     };
     optimise.automatic = true;
+
   };
 
   hardware.enableRedistributableFirmware = true;
