5b92b7c6aa6adb6cfcb64469b03543de19c16166
Baitinq <manuelpalenzuelamerino@gmail.com>
2026-01-12 00:11:32 +0100

    Fonts: Update to noto-fonts-color-emoji

--- modules/fonts/default.nix
+++ modules/fonts/default.nix
@@ -7,7 +7,7 @@
     packages = with pkgs; [
       noto-fonts
       noto-fonts-cjk-sans
-      noto-fonts-emoji
+      noto-fonts-color-emoji
       nerd-fonts.inconsolata-lgc
       nerd-fonts.noto
       (pkgs.callPackage ./berkeley-mono.nix {})
