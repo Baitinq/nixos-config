a820286b02b7576ca27558fe600504bd036395f6
Baitinq <manuelpalenzuelamerino@gmail.com>
2026-08-30 21:57:28 +0200

    nix: add CUDA binary cache

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -277,10 +277,12 @@
       substituters = [
         "https://nix-community.cachix.org"
         "https://cache.numtide.com"
+        "https://cache.nixos-cuda.org"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
+        "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
       ];
       access-tokens = "github.com=${secrets.github_access_token}";
     };
